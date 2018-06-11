1b50d16d9c0075225b3910f3aadecaff66babc65
LiuZP <toliuzp@gmail.com>
2018-06-11 15:26:19 +0800

    Upgrade to python3

--- threedoors/new_threedoor_v1/new_threedoors.py
+++ threedoors/new_threedoor_v1/new_threedoors.py
@@ -6,8 +6,8 @@ win_times = 0
 lose_times = 0
 counter = 0
 while running:
-    luckydoor = Doors()
-    playera = Player()
+    luckydoor = door.Doors()
+    playera = player.Player()
 
     luckydoor.gen_award_door()
     playera.make_choose()
@@ -17,14 +17,14 @@ while running:
         print('The prize is behind the {} door.'.format(luckydoor.award_door))
         print('Player chose the {} door at the begining.'.format(playera.choose))
         print('Player changing their chose now')
-        print('Player''s new choose is ' + playera.new_choose + ' door.')
+        print('Player''s new choose is ' + str(playera.new_choose) + ' door.\n')
         counter = counter + 1
         if counter == 1000:
             running = False
 
         if luckydoor.award_door == playera.new_choose:
-            win = win_times +1
+            win_times = win_times +1
         else:
-            lose = lose_times +1
-print('After change, player wins ' + win_times + ' times')
-print('After change, player lose ' + lose_times + ' times')
+            lose_times = lose_times +1
+print('After change, player wins ' + str(win_times) + ' times')
+print('After change, player lose ' + str(lose_times) + ' times')
--- threedoors/new_threedoor_v1/player.py
+++ threedoors/new_threedoor_v1/player.py
@@ -1,9 +1,6 @@
 import random
 
 class Player(object):
-    """def __init__(self,choose,new_choose):
-        self.choose = choose
-        self.new_choose = new_choose"""
 
     def make_choose(self):
         self.choose = random.randint(1, 3)
@@ -11,13 +8,11 @@ class Player(object):
     def change_choose(self, award_door:int) -> None:
         keep_try_ind = True
         while keep_try_ind:
-            next_choose = random.randint(1, 3)
-            if self.choose != next_choose and award_door != next_choose :
+            deserted_choose = random.randint(1, 3)
+            if self.choose != deserted_choose and award_door != deserted_choose :
                 keep_try_ind = False
-                # self.choose = next_choose
-                self.new_choose = next_choose
-                """new_match_ind = True
+                new_match_ind = True
                 while new_match_ind:
                     self.new_choose = random.randint(1, 3)
-                    if next_choose <> self.new_choose:
-                        new_match_ind = False"""
+                    if deserted_choose != self.new_choose:
+                        new_match_ind = False
