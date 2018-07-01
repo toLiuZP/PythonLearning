31d9c7b5b66822ad7f1d38af8c67bf04f5984257
LiuZP <toliuzp@gmail.com>
2018-07-01 18:41:39 +0800

    Learn exception

--- HeadFirst/webapp/DBcm.py
+++ HeadFirst/webapp/DBcm.py
@@ -1,17 +1,31 @@
 import mysql.connector
 
+class DBConnectionError(Exception):
+    pass
+
+class CredentialsError(Exception):
+    pass
+
+class SQLError(Exception):
+    pass
+
 class UseDataBase:
 
     def __init__(self, config:dict) -> None:
         self.configuration = dict(config)
 
     def __enter__(self) -> 'cursor':
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise DBConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err)
         
 
-    def __exit__(self,exc_type, exc_value, exc_trace):
+    def __exit__(self,exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
--- HeadFirst/webapp/vsearch4web.py
+++ HeadFirst/webapp/vsearch4web.py
@@ -1,37 +1,25 @@
 from flask import Flask, render_template, request,redirect, escape
 import vsearch
-import mysql.connector
+from DBcm import UseDataBase, DBConnectionError, CredentialsError, SQLError
 
 app = Flask(__name__)
-
-
-"""
-def log_request(req:'flask_request', res:str) -> None:
-    with open('vsearch.log','a') as log:
-        print(req.form,req.remote_addr,req.user_agent,res,file=log,sep='|')
-"""
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                            'user': 'vsearch',
+                            'password':'test',
+                            'database':'vsearchlogDB',
+                            }
 
 
 def log_request(req:'flask_request', res:str) -> None:
     """Log details of the web request and the results."""
-    dbconfig = { 'host': '127.0.0.1',
-                'user': 'vsearch',
-                'password': 'test',
-                'database': 'vsearchlogDB',}
-
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-
-    _SQL = """ insert into log
-                (phrase, letters, ip, browser_string, results) values (%s,%s,%s,%s,%s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
-                            req.form['letters'],
-                            req.remote_addr,
-                            req.user_agent.browser,
-                            res,))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with UseDataBase(app.config['dbconfig']) as cursor:
+        _SQL = """ insert into log
+                    (phrase, letters, ip, browser_string, results) values (%s,%s,%s,%s,%s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                                req.form['letters'],
+                                req.remote_addr,
+                                req.user_agent.browser,
+                                res,))
 
 
 @app.route('/search4', methods = ['POST'])
@@ -52,18 +40,28 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-    contents=[]
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr','User_agent','Results')
-    return render_template('viewlog.html',
-                            the_title='View Log',
-                            the_row_titles=titles,
-                            the_data=contents,)
-    
+    try:
+        """Display the contents of the log file as a HTML table."""
+        with UseDataBase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+
+            titles = ('Phrase','Letters', 'Remote_addr','User_agent','Results')
+            return render_template('viewlog.html',
+                                    the_title='View Log',
+                                    the_row_titles=titles,
+                                    the_data=contents,)
+    except DBConnectionError as err:
+        print('Is your database swithed on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:',str(err))
+    except Exception as err:
+        print ('Something went wrong:', str(err))
+    return "Error"
+        
 
 if __name__ == '__main__':
     app.run(debug=True) 
