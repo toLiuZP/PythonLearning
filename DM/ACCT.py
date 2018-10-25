ea2163c2dfc41d62e18f076238c41d519bd027a9
LiuZP <toliuzp@gmail.com>
2018-10-25 17:54:57 +0800

    v1

--- DM/ACCT.py
+++ DM/ACCT.py
@@ -1,5 +1,23 @@
-config = {'host': 'mwdbws0224.dev.activenetwork.com',
+dev_co = {'host': 'mwdbws0224.dev.activenetwork.com',
                             'user': 'active\zliu1',
                             'password':'Jiaoxkm3',
                             'database':'CO_HF_MART',
+                            }
+
+dev_ks = {'host': 'mwdbws0224.dev.activenetwork.com',
+                            'user': 'active\zliu1',
+                            'password':'Jiaoxkm3',
+                            'database':'KS_HF_MART',
+                            }
+
+dev_ms = {'host': 'mwdbws0224.dev.activenetwork.com',
+                            'user': 'active\zliu1',
+                            'password':'Jiaoxkm3',
+                            'database':'MS_HF_MART',
+                            }
+
+dev_sales = {'host': 'mwdbws0224.dev.activenetwork.com',
+                            'user': 'active\zliu1',
+                            'password':'Jiaoxkm3',
+                            'database':'ASPIRA_SALES_DATA_MART',
                             }
--- DM/Main.py
+++ DM/Main.py
@@ -1,28 +1,43 @@
-from SQLSERVERDB import UseSqlserverDB, DBConnectionError, CredentialsError, SQLError
-import ACCT
+import Scrub
+import random
+##queryProduct = "SELECT TOP 1 PRODUCT_NM ,PRODUCT_DSC ,PRODUCT_CATEGORY_NM ,PRODUCT_GROUP_NM ,PRODUCT_CATEGORY_NM ,PRODUCT_CLASS_NM ,PRODUCT_SUBCATEGORY_NM  FROM D_PRODUCT WITH(NOLOCK)"
 
-def main(sql_txt):
+def updateProduct():
 
-    try:
-        """Display the contents of the log file as a HTML table."""
-        with UseSqlserverDB(ACCT.config) as cursor:
+        queryProduct = "SELECT PRODUCT_CD, PRODUCT_NM ,PRODUCT_DSC ,PRODUCT_CATEGORY_NM ,PRODUCT_GROUP_NM ,PRODUCT_CATEGORY_NM ,PRODUCT_SUBCATEGORY_NM, AWO_ID  FROM D_PRODUCT WITH(NOLOCK)"
+
+        print("Query source begin")
+
+        sample_result = Scrub.querySampleProduct(queryProduct)
+        target_result = Scrub.queryTargetProduct(queryProduct)
+
+        count = 0 
+
+        for item in target_result:
             
-            cursor.execute(sql_txt)
-            contents = cursor.fetchall()
+            awo_id = item[7]
+            ##print(awo_id)
+            match_ind = True
+            while match_ind:
+                sourceid = random.randint(0,len(sample_result)-1)
+                if item[3] == sample_result[sourceid][3] and item[5] == sample_result[sourceid][5] and item[6] == sample_result[sourceid][6]:
+                    Scrub.scrubProduct(awo_id,sample_result[sourceid])
+                    del sample_result[sourceid]
+                    print(count)
+                    count = count + 1
+                    match_ind = False
 
-            print (contents)
-
-    except DBConnectionError as err:
-        print('Is your database swithed on? Error:', str(err))
-    except CredentialsError as err:
-        print('User-id/Password issues. Error:', str(err))
-    except SQLError as err:
-        print('Is your query correct? Error:',str(err))
-    except Exception as err:
-        print ('Something went wrong:', str(err))
-    return "Error"
 
 if __name__ == '__main__':
 
-    _TEST_SQL = "SELECT TOP 2 * FROM [CO_HF_MART].[dbo].[D_ADDRESS] WITH(NOLOCK)"
-    main(_TEST_SQL)
+
+    ##Scrub.backupProduct()
+
+
+    ##Scrub.queryProduct(queryProduct)
+    updateProduct()
+
+
+    
+
+    
