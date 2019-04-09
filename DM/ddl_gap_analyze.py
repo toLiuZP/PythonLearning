c1e4c14653ffc57f7bca2f5ba0ddf216d01d091b
LiuZP <toliuzp@gmail.com>
2019-04-09 11:30:00 +0800

    Finished source data verification.

--- DM/ddl_gap_analyze.py
+++ DM/ddl_gap_analyze.py
@@ -33,12 +33,12 @@ if __name__ == '__main__':
     query = "SELECT table_schema, table_name, column_name, ordinal_position , data_type, COALESCE(character_maximum_length,numeric_precision,datetime_precision) data_length, numeric_scale, is_nullable FROM information_schema.columns WHERE table_schema = 'dbo' AND table_name NOT LIKE 'MSpeer_%' AND table_name NOT LIKE 'MSpub_%' AND table_name NOT LIKE 'syncobj_0x%' AND table_name NOT LIKE 'sysarticle%' AND table_name NOT LIKE 'sysextendedarticlesview' AND table_name NOT LIKE 'syspublications' AND table_name <> 'sysreplservers' AND table_name <> 'sysreplservers' AND table_name <> 'sysschemaarticles' AND table_name <> 'syssubscriptions' AND table_name <> 'systranschemas' ORDER BY table_name"
     writer = pd.ExcelWriter('.\output\gap' + re.sub(r'[^0-9]','',str(datetime.datetime.now())) + '.xlsx')
     
-    raw1 = DB.query_db_pandas(query, acct.UAT_CAMPING_SALES)
-    raw2 = DB.query_db_pandas(query, acct.UAT_TX_CAMPING_MART)
-    #raw3 = DB.query_db_pandas(query, acct.PROD_CO_HF_MART)
+    raw1 = DB.query_db_pandas(query, acct.QA_CO_HF_MART)
+    raw2 = DB.query_db_pandas(query, acct.UAT_CO_HF_MART)
+    raw3 = DB.query_db_pandas(query, acct.PROD_CO_HF_MART)
 
-    #merge(raw1,raw2,'CO',writer,raw3)
-    merge(raw1,raw2,'TX',writer)
+    merge(raw1,raw2,'CO',writer,raw3)
+    #merge(raw1,raw2,'KS',writer)
     
 
 
--- DM/tool/oracle_tool.py
+++ DM/tool/oracle_tool.py
@@ -2,6 +2,7 @@ import cx_Oracle as oracle
 
 
 def has_row(query, cursor):
+    print(query)
     cursor.execute(query)
     row = cursor.fetchall()
     if len(row) == 0:
