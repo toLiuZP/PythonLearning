139c1c632107309fe5498cd80b6835a388f86c6b
LiuZP <toliuzp@gmail.com>
2019-04-23 22:12:43 +0800

    add view check.

--- DM/ddl_analyze_full.py
+++ DM/ddl_analyze_full.py
@@ -22,7 +22,7 @@ ddl_sheet = workbook.get_sheet_by_name('DDL')
 
 check_list = ['CO_HF_MART','KS_HF_MART']
 
-def merge(dev, qa, uat, sheet, prod='none'):
+def merge_ddl(dev, qa, uat, sheet, prod='none'):
 
     dev = dev.fillna('null')
     qa = qa.fillna('null')
@@ -46,12 +46,8 @@ def merge(dev, qa, uat, sheet, prod='none'):
                 and col[5] == col[10] == col[15]  == col[20] and col[6] == col[11] == col[16]  == col[21]:
                 '''Don't compare postion col[2] == col[7] == col[12] == col[17] and'''
                 gap = gap.drop(index)
-    
 
     gap = gap.reset_index(drop=True)
-
-    
-
     for index, col in gap.iterrows():
         for i in range(0, len(col), 1):
             sheet.cell(row=index+3,column=i+1).value = col[i]
@@ -64,23 +60,75 @@ def merge(dev, qa, uat, sheet, prod='none'):
             elif i in (17,18,19,20,21):
                 sheet.cell(row=index+3,column=i+1).fill=sty.PatternFill(fill_type='solid',fgColor="5B7190")
 
+def merge_view(dev, qa, uat, sheet, prod='none'):
+
+    dev = dev.fillna('null')
+    qa = qa.fillna('null')
+    uat = uat.fillna('null')
+
+    gap = pd.merge(dev, qa, on = ['type','name'], how='outer')
+    gap = pd.merge(gap, uat, on = ['type','name'], how='outer')
+
+    if isinstance(prod, str):
+        for index, col in gap.iterrows():
+            if col[3] == col[4] == col[5]:
+                gap = gap.drop(index)
+                
+    else:
+        for index, col in gap.iterrows():
+            prod = prod.fillna('null')
+            gap = pd.merge(gap, prod, on = ['type','name'], how='outer')
+            if  col[3] == col[4] == col[5]  == col[6]:
+                gap = gap.drop(index)
+
+    gap = gap.reset_index(drop=True)
+    for index, col in gap.iterrows():
+        for i in range(0, len(col), 1):
+            sheet.cell(row=index+3,column=i+1).value = col[i]
+            if i == 3:
+                sheet.cell(row=index+3,column=i+1).fill=sty.PatternFill(fill_type='solid',fgColor="C28EEA")
+            elif i == 4:   
+                sheet.cell(row=index+3,column=i+1).fill=sty.PatternFill(fill_type='solid',fgColor="DAC45E")
+            elif i == 5:
+                sheet.cell(row=index+3,column=i+1).fill=sty.PatternFill(fill_type='solid',fgColor="68AE59")
+            elif i == 6:
+                sheet.cell(row=index+3,column=i+1).fill=sty.PatternFill(fill_type='solid',fgColor="5B7190")
+
+def check_ddl(workbook,ddl_sheet,contract):
+
+    query = "SELECT table_name, column_name, ordinal_position , data_type, COALESCE(character_maximum_length,numeric_precision,datetime_precision) data_length, numeric_scale, is_nullable FROM " + contract + ".information_schema.columns WHERE table_schema = 'dbo' AND table_name NOT LIKE 'MSpeer_%' AND table_name NOT LIKE 'MSpub_%' AND table_name NOT LIKE 'syncobj_0x%' AND table_name NOT LIKE 'sysarticle%' AND table_name NOT LIKE 'sysextendedarticlesview' AND table_name NOT LIKE 'syspublications' AND table_name <> 'sysreplservers' AND table_name <> 'sysreplservers' AND table_name <> 'sysschemaarticles' AND table_name <> 'syssubscriptions' AND table_name <> 'systranschemas' ORDER BY table_name"
+
+    new_sheet = workbook.copy_worksheet(ddl_sheet)
+    new_sheet.title = contract + "_DDL"
+    dev = DB.query_db_pandas(query, acct.DEV_CO_HF_MART)
+    qa = DB.query_db_pandas(query, acct.QA_CO_HF_MART)
+    uat = DB.query_db_pandas(query, acct.UAT_CO_HF_MART)
+    prod = DB.query_db_pandas(query, acct.PROD_CO_HF_MART)
+
+    #merge_ddl(dev,qa,uat,new_sheet)
+    merge_ddl(dev,qa,uat,new_sheet,prod)
+
+def check_view(workbook,ddl_sheet,contract):
+
+    query = "SELECT CASE a.[type] WHEN 'P' THEN 'Stored Procedures' WHEN 'V' THEN 'Views' WHEN 'AF' THEN 'Aggregate function' END AS 'Type', a.name, b.[definition] FROM " + contract + ".sys.all_objects a, " + contract + ".sys.sql_modules b WHERE a.is_ms_shipped=0 AND a.object_id = b.object_id AND a.[type] IN ('P','V','AF') order by a.[type], a.[name] ASC"
+
+    new_sheet = workbook.copy_worksheet(ddl_sheet)
+    new_sheet.title = contract + "_VIEW"
+    dev = DB.query_db_pandas(query, acct.DEV_CO_HF_MART)
+    qa = DB.query_db_pandas(query, acct.QA_CO_HF_MART)
+    uat = DB.query_db_pandas(query, acct.UAT_CO_HF_MART)
+    prod = DB.query_db_pandas(query, acct.PROD_CO_HF_MART)
+
+    #merge_ddl(dev,qa,uat,new_sheet)
+    merge_view(dev,qa,uat,new_sheet,prod)
+
+
 if __name__ == '__main__':
 
     # US contracts:
-
     for contract in check_list:
-
-        query = "SELECT table_name, column_name, ordinal_position , data_type, COALESCE(character_maximum_length,numeric_precision,datetime_precision) data_length, numeric_scale, is_nullable FROM " + contract + ".information_schema.columns WHERE table_schema = 'dbo' AND table_name NOT LIKE 'MSpeer_%' AND table_name NOT LIKE 'MSpub_%' AND table_name NOT LIKE 'syncobj_0x%' AND table_name NOT LIKE 'sysarticle%' AND table_name NOT LIKE 'sysextendedarticlesview' AND table_name NOT LIKE 'syspublications' AND table_name <> 'sysreplservers' AND table_name <> 'sysreplservers' AND table_name <> 'sysschemaarticles' AND table_name <> 'syssubscriptions' AND table_name <> 'systranschemas' ORDER BY table_name"
-
-        new_ddl_sheet = workbook.copy_worksheet(ddl_sheet)
-        new_ddl_sheet.title = contract + "_DDL"
-        dev = DB.query_db_pandas(query, acct.DEV_CO_HF_MART)
-        qa = DB.query_db_pandas(query, acct.QA_CO_HF_MART)
-        uat = DB.query_db_pandas(query, acct.UAT_CO_HF_MART)
-        #prod = DB.query_db_pandas(query, acct.PROD_CO_HF_MART)
-
-        merge(dev,qa,uat,new_ddl_sheet)
-
+        check_ddl(workbook,ddl_sheet,contract)
+        check_view(workbook,ddl_sheet,contract)
     
 workbook.remove_sheet(workbook.get_sheet_by_name('DDL'))
 workbook.remove_sheet(workbook.get_sheet_by_name('VIEW'))
