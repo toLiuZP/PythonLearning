918b0c560c071a7986d13222c4887a6e238eb647
LiuZP <toliuzp@gmail.com>
2019-05-31 16:45:20 +0800

    Update to remove AB for monitor.

---
+++ DM/seed/orginal.sql
@@ -0,0 +1,18 @@
+﻿USE NJMigraQA;
+GO
+
+SELECT TOP 100 LA.* 
+FROM dbo.[Transaction] AS T WITH(NOLOCK) 
+INNER JOIN dbo.TransactionHeader AS TH WITH(NOLOCK) 
+    ON T.TransactionID = TH.TransactionID
+INNER JOIN dbo.TransactionDetail TD WITH(NOLOCK) 
+    ON TD.TransactionHeaderID = TH.TransactionHeaderID
+INNER JOIN dbo.TransactionType TT WITH(NOLOCK) 
+    ON TD.TransactionTypeID = TT.TransactionTypeID
+INNER JOIN dbo.LicenseAction LA WITH(NOLOCK) ON LA.TransactionDetailID = TD.TransactionDetailID
+	WHERE TD.TransactionTypeID = 5
+
+
+SELECT TOP 100 
+	* 
+FROM F_TRANSACTION_DETAIL FTD WITH(NOLOCK)
